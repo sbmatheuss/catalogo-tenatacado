d63168c1d6c378ab87343dfea4c939feab02567d
sbmatheuss <sbmatheus20@gmail.com>
2026-04-28 10:47:31 -0300

    fix: update QR code URL to Netlify

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import { QRCodeSVG } from 'qrcode.react';
 import './Footer.css';
 
-const SITE_URL = 'https://sbmatheuss.github.io/catalogo-tenatacado';
+const SITE_URL = 'https://tenatacado.netlify.app/';
 
 export function Footer() {
   return (
