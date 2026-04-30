3b353786b3f8a0a18b39274827ddfbd7d222bf22
sbmatheuss <sbmatheus20@gmail.com>
2026-04-30 09:09:25 -0300

    fix: update QR code URL to Cloudflare Pages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import { QRCodeSVG } from 'qrcode.react';
 import './Footer.css';
 
-const SITE_URL = 'https://catalogo-tenatacado.pagework.workers.dev/';
+const SITE_URL = 'https://catalogo-tenatacado.pages.dev/';
 
 export function Footer() {
   return (
